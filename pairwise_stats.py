56ab0a0683367fcc71d865036397da9cb2d2b5f7
JD-X1 <jduque2@ucmerced.edu>
2023-03-06 13:32:40 -0800

    test files upload

--- pairwise_stats.py
+++ pairwise_stats.py
@@ -6,19 +6,19 @@ import dendropy
 
 #print(sys.path)
 path_root = pathlib.Path(__file__).parents[0]
-sys.path.append(str(path_root) + '/moon_quiver')
-from moon_quiver.moon_dragon import *
+sys.path.append(str(path_root) + '/modules')
+from modules.moon_dragon import *
 
-path_list = open("/home/jduque2/moon_logic/bs_path", 'r')
+path_list = open("tre_path", 'r')
 header = "base_tree\t"+"genTree\t"+"falsePosBip\t"+"falseNegBip\t"+"uRF\t"+"wRF\t"+"sackinBase\t"+"sackinGen"+"\n"
 print(header)
 for line in path_list:
 	outline = ""
-	t1 = "/home/jduque2/moon_logic/example_out/simtree.tre"
+	t1 = "simtree.tre"
 	t2=line.rstrip("\n").split("/")[4]
 	tns,t1,t2 = pair_importer(t1,line.rstrip("\n"))
 	tns,t1,t2 = standardize_namespace(tns, t1, t2)
-	tns,t1,t2 = reroot_pair(tns, t1, t2, "SRR498373,SRR500494,SRR498369,SRR500493")
+	tns,t1,t2 = reroot_pair(tns, t1, t2, ["SRR498373","SRR500494","SRR498369","SRR500493"])
 	print('adjusted:')
 	print(t1.as_ascii_plot())
 	print(t2.as_ascii_plot())
--- tests/example_test.py
+++ tests/example_test.py
@@ -1,51 +1,51 @@
+#! usr/bin/python3.11
 import dendropy
 import unittest
 
 def reroot_pair(tns, t1, t2, root_taxa):
-    """Function to make sure two trees have same outgroup.
-    Args:
-    tns = dendropy taxon_namespace for both trees
-    t1 = dendropy tree object
-    t2 = dendropy tree object
-    root_taxa = list of taxon names
-    """
-    # rerooting at input mrca edge object need to standardize namespace prior to this
-    mrca1 = t1.mrca(taxon_labels=root_taxa)
-    assert mrca1
-    mrca2 = t2.mrca(taxon_labels=root_taxa)
-    assert mrca2
-    t1.to_outgroup_position(mrca1, update_bipartitions=True)
-    t2.to_outgroup_position(mrca2, update_bipartitions=True)
-    return tns,t1,t2
-
-tns = dendropy.TaxonNamespace()
-tre1 = "/home/josue/Desktop/op_lunar_trees/gi_bestBiCon.tre"
-tree1 = dendropy.Tree.get(
-        file=open(tre1, 'r'),
-        schema="newick",
-        taxon_namespace=tns,
-        preserve_underscores=True,
-        )
-tree1.prune_taxa_with_labels(["ref_gi"])
-print(tree1.as_ascii_plot())
-#example_tree1 = "((A,B),(C,D,E,F));"
-#tree1 = dendropy.Tree.get(data = example_tree1, schema = "newick")
-
-root_taxa = ["SRR498373","SRR500494","SRR498369","SRR500493"]## Multiple outgroup taxa
-
-##Check single tree root works
-t_mrca1 = tree1.mrca(taxon_labels=root_taxa)
-#outgroup_node = tree1.find_node_with_taxon_label(root_taxa)
-og_leaves = [leaf.taxon.label for leaf in t_mrca1.leaf_iter()]
-
-#unittest.assertCountEqual(og_leaves, root_taxa)
+	"""Function to make sure two trees have same outgroup.
+	Args:
+	tns = dendropy taxon_namespace for both trees
+	t1 = dendropy tree object
+	t2 = dendropy tree object
+	root_taxa = list of taxon names
+	"""
+	# rerooting at input mrca edge object need to standardize namespace prior to this
+	mrca1 = t1.mrca(taxon_labels=root_taxa)
+	assert mrca1
+	mrca2 = t2.mrca(taxon_labels=root_taxa)
+	assert mrca2
+	t1.reroot_at_node(mrca1, update_bipartitions=True)
+	t2.reroot_at_node(mrca2, update_bipartitions=True)
+	return tns,t1,t2
 
 
-print(tree1.as_ascii_plot())
+class TestRootPair(unittest.TestCase):
+	def test_mrcaLeafEquivalence(self):
+		tns = dendropy.TaxonNamespace()
+		tre1 = "/home/josue/Desktop/op_lunar_trees/MoonTrees/bestTrees/SRR498444_bestBiCon.tre"
+		tree1 = dendropy.Tree.get(
+			file=open(tre1, 'r'),
+			schema="newick",
+			taxon_namespace=tns,
+			preserve_underscores=True
+			)
+		tre2 = "/home/josue/Desktop/op_lunar_trees/MoonTrees/simree.tre"
+		tree2 = dendropy.Tree.get(
+			file=open(tre1, 'r'),
+			schema="newick",
+			taxon_namespace=tns,
+			preserve_underscores=True
+			)
+		tree1.prune_taxa_with_labels(["ref_SRR498444"])
+		root_taxa = ["SRR498373","SRR500494","SRR498369","SRR500493"]## Multiple outgroup taxa
+		t_mrca1 = tree1.mrca(taxon_labels=root_taxa)
+		t_mrca2 = tree2.mrca(taxon_labels=root_taxa)
+		ori1_leaves = [leaf.taxon.label for leaf in t_mrca1.leaf_iter()]
+		ori2_leaves = [leaf.taxon.label for leaf in t_mrca2.leaf_iter()]
+		self.assertCountEqual(ori1_leaves, root_taxa)
 
-tree1.reroot_at_node(t_mrca1, update_bipartitions=False)
 
-print(tree1.as_ascii_plot())
 
 """
 
